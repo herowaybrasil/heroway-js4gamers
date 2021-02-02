e0c5cbfa3840d5601f5575053f2b2c703e1d3b44
Gtosta96 <gabrieltosta3@gmail.com>
2021-02-02 14:58:35 -0300

    v2 - refactored

--- src/scripts.js
+++ src/scripts.js
@@ -9,6 +9,8 @@ const DIRECTIONS = {
   left: "ArrowLeft",
 };
 
+// -----
+
 const root = document.documentElement;
 root.style.setProperty("--tile-size", `${TILE_SIZE}px`);
 root.style.setProperty("--helmet-offset", `${HELMET_OFFSET}px`);
@@ -16,141 +18,147 @@ root.style.setProperty("--game-size", `${GAME_SIZE}px`);
 
 // -----
 
-function getNewPosition(buttonPressed, position) {
-  switch (buttonPressed) {
-    case DIRECTIONS.up:
-      return { left: position.left, top: position.top - TILE_SIZE };
+function createBoard() {
+  const htmlBoardElement = document.getElementById("board");
+  const boardElements = [];
 
-    case DIRECTIONS.right:
-      return { left: position.left + TILE_SIZE, top: position.top };
+  function createElement(options) {
+    const { className, top, left } = options;
 
-    case DIRECTIONS.down:
-      return { left: position.left, top: position.top + TILE_SIZE };
+    const htmlElement = document.createElement("div");
+    htmlElement.className = className;
+    htmlElement.style.top = `${top}px`;
+    htmlElement.style.left = `${left}px`;
+    htmlBoardElement.appendChild(htmlElement);
+  
+    boardElements.push({ className, currentPosition: { top, left } });
+    const boardElement = boardElements[boardElements.length - 1];
 
-    case DIRECTIONS.left:
-      return { left: position.left - TILE_SIZE, top: position.top };
+    function getNewPosition(buttonPressed, position) {
+      switch (buttonPressed) {
+        case DIRECTIONS.up:
+          return { left: position.left, top: position.top - TILE_SIZE };
+    
+        case DIRECTIONS.right:
+          return { left: position.left + TILE_SIZE, top: position.top };
+    
+        case DIRECTIONS.down:
+          return { left: position.left, top: position.top + TILE_SIZE };
+    
+        case DIRECTIONS.left:
+          return { left: position.left - TILE_SIZE, top: position.top };
+    
+        default:
+          position;
+      }
+    }
 
-    default:
-      position;
-  }
-}
-
-const allElements = [];
-function createGameElement(options) {
-  const { className, top, left } = options;
-
-  allElements.push({
-    className,
-    currentPosition: { top, left },
-  });
-  const element = allElements[allElements.length - 1];
-
-  const htmlElement = document.createElement("div");
-  htmlElement.className = className;
-  htmlElement.style.top = `${top}px`;
-  htmlElement.style.left = `${left}px`;
-
-  function validateMoviment(position, conflict) {
-    return (
-      position.left >= 48 &&
-      position.left <= 864 &&
-      position.top >= 96 &&
-      position.top <= 816 &&
-      conflict?.className !== "forniture"
-    );
-  }
-
-  function getMovimentConflict(position) {
-    return allElements.find((el) => {
+    function getMovimentConflict(position) {
+      return boardElements.find((el) => {
+        return (
+          el.currentPosition.top === position.top &&
+          el.currentPosition.left === position.left
+        );
+      });
+    }
+  
+    function validateMoviment(position, conflict) {
       return (
-        el.currentPosition.top === position.top &&
-        el.currentPosition.left === position.left
+        position.left >= 48 &&
+        position.left <= 864 &&
+        position.top >= 96 &&
+        position.top <= 816 &&
+        conflict?.className !== "forniture"
       );
+    }
+  
+    function validateConflicts(conflict) {
+      function finishGame(message) {
+        setTimeout(() => {
+          alert(message)
+          window.location.reload();
+        }, 25);
+      }
+
+      if (conflict && conflict.className !== "forniture") {
+        if (boardElement.className === "hero") {
+          if (
+            conflict.className === "mini-demon" ||
+            conflict.className === "trap"
+          ) {
+            finishGame("You Died");
+          }
+  
+          if (conflict.className === "chest") {
+            finishGame("You Win");
+          }
+        }
+  
+        if (boardElement.className === "mini-demon" && conflict.className === "hero") {
+          finishGame("You Died");
+        }
+      }
+    }
+  
+    function move(buttonPressed) {
+      const newPosition = getNewPosition(buttonPressed, boardElement.currentPosition);
+      const conflict = getMovimentConflict(newPosition);
+      const isValidMoviment = validateMoviment(newPosition, conflict);
+  
+      if (isValidMoviment) {
+        validateConflicts(conflict);
+  
+        boardElement.currentPosition = newPosition;
+        htmlElement.style.top = `${newPosition.top}px`;
+        htmlElement.style.left = `${newPosition.left}px`;
+      }
+    }
+  
+    return { move };
+  }
+
+  function createHero(options) {
+    const { top, left } = options;
+    const gameElement = createElement({
+      className: "hero",
+      top: top,
+      left: left,
+    });
+  
+    window.addEventListener("keydown", (event) => {
+      gameElement.move(event.key);
     });
   }
 
-  function validateConflicts(conflict) {
-    if (conflict && conflict.className !== "forniture") {
-      if (element.className === "hero") {
-        if (
-          conflict.className === "mini-demon" ||
-          conflict.className === "trap"
-        ) {
-          setTimeout(() => alert("You Died"), 10);
-        }
-
-        if (conflict.className === "chest") {
-          setTimeout(() => alert("You Win"), 10);
-        }
-      }
-
-      if (element.className === "mini-demon" && conflict.className === "hero") {
-        setTimeout(() => alert("You Died"), 10);
-      }
-    }
+  function createEnemy(options) {
+    const { top, left } = options;
+    const gameElement = createElement({
+      className: "mini-demon",
+      top: top,
+      left: left,
+    });
+  
+    window.setInterval(() => {
+      const directionsValue = Object.values(DIRECTIONS);
+      const randomIndex = Math.floor(Math.random() * directionsValue.length);
+      const randomDirection = directionsValue[randomIndex];
+  
+      gameElement.move(randomDirection);
+    }, 2000);
   }
 
-  function move(buttonPressed) {
-    const newPosition = getNewPosition(buttonPressed, element.currentPosition);
-    const conflict = getMovimentConflict(newPosition);
-    const isValidMoviment = validateMoviment(newPosition, conflict);
-
-    if (isValidMoviment) {
-      validateConflicts(conflict);
-
-      element.currentPosition = newPosition;
-      htmlElement.style.top = `${newPosition.top}px`;
-      htmlElement.style.left = `${newPosition.left}px`;
-    }
-  }
-
-  appendToBoard(htmlElement);
-
-  return { element: htmlElement, move };
+  return { createElement, createHero, createEnemy };
 }
 
-function createGameHero(options) {
-  const { top, left } = options;
-  const gameElement = createGameElement({
-    className: "hero",
-    top: top,
-    left: left,
-  });
+const board = createBoard();
 
-  window.addEventListener("keydown", (event) => {
-    gameElement.move(event.key);
-  });
-}
+board.createElement({ className: "forniture", top: TILE_SIZE * 2, left: TILE_SIZE * 3 });
+board.createElement({ className: "forniture", top: TILE_SIZE * 2, left: TILE_SIZE * 8 });
+board.createElement({ className: "forniture", top: TILE_SIZE * 2, left: TILE_SIZE * 16 });
+board.createElement({ className: "forniture", top: TILE_SIZE * 17, left: TILE_SIZE * 2 });
 
-function createGameEnemy(options) {
-  const { top, left } = options;
-  const gameElement = createGameElement({
-    className: "mini-demon",
-    top: top,
-    left: left,
-  });
+board.createElement({ className: "trap", top: TILE_SIZE * 6, left: TILE_SIZE * 2 });
+board.createElement({ className: "chest", top: TILE_SIZE * 8, left: TILE_SIZE * 2 });
 
-  window.setInterval(() => {
-    const directionsValue = Object.values(DIRECTIONS);
-    const randomIndex = Math.floor(Math.random() * directionsValue.length);
-    const randomDirection = directionsValue[randomIndex];
-
-    gameElement.move(randomDirection);
-  }, 2000);
-}
-
-function appendToBoard(child) {
-  const boardElement = document.getElementById("board");
-  boardElement.appendChild(child);
-}
-
-createGameHero({ top: TILE_SIZE * 16, left: TILE_SIZE * 2 });
-createGameEnemy({ top: TILE_SIZE * 4, left: TILE_SIZE * 2 });
-
-createGameElement({ className: "forniture", top: TILE_SIZE * 2, left: TILE_SIZE * 3 });
-createGameElement({ className: "forniture", top: TILE_SIZE * 2, left: TILE_SIZE * 8 });
-createGameElement({ className: "forniture", top: TILE_SIZE * 2, left: TILE_SIZE * 16 });
-createGameElement({ className: "forniture", top: TILE_SIZE * 17, left: TILE_SIZE * 2 });
-
-createGameElement({ className: "trap", top: TILE_SIZE * 6, left: TILE_SIZE * 2 });
-createGameElement({ className: "chest", top: TILE_SIZE * 8, left: TILE_SIZE * 2 });
+board.createHero({ top: TILE_SIZE * 16, left: TILE_SIZE * 2 });
+board.createEnemy({ top: TILE_SIZE * 4, left: TILE_SIZE * 2 });
